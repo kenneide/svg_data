4264b91227d722c6fbbba34b24fc117b2d04b0e7
Alan O Cinneide <alan.ocinneide@gmail.com>
2020-01-26 18:32:57 +0100

    Improve svg element handling
    Add factory and class for svg elements

---
+++ svg_element_factory.py
@@ -0,0 +1,26 @@
+from svg_path import SvgPath
+
+
+class SvgGraphicsElementsFactoryNotImplementedError(NotImplementedError):
+	pass
+	
+
+class SvgGraphicsElementFactory():
+	def __init__(self):
+		self.KNOWN_GRAPHICS = ['path']
+		
+	def get_graphics(self, type, attrib, transform):
+		if type in self.KNOWN_GRAPHICS:			
+			id = None
+			if 'id' in attrib.keys():
+				id = attrib['id']
+				
+			if type == 'path':
+				pathdata = None
+				if 'id' in attrib.keys():
+					id = attrib['id']
+				if 'd' in attrib.keys():
+					pathdata = attrib['d']
+				return SvgPath(id=id, pathdata=pathdata, transform=transform)
+		else:
+			raise SvgGraphicsElementsFactoryNotImplementedError()

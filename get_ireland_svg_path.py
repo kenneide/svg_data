370754653a848d72995af5b0cac3c9920e9cf34a
Alan O Cinneide <alan.ocinneide@gmail.com>
2020-01-26 18:34:17 +0100

    Rewrite test script

--- get_ireland_svg_path.py
+++ get_ireland_svg_path.py
@@ -1,38 +1,13 @@
-from bs4 import BeautifulSoup
-from svg_path import SvgPath
-from svg_transform import SvgTransform
-import csv
+from svg_handle import SvgHandle
 import matplotlib.pyplot as plt
 
 filename = 'ireland.svg'
-
-with open(filename, 'r', encoding='utf-8') as svgfile:
-	htmldata = BeautifulSoup(svgfile, 'html5lib')
 	
-svgdata = htmldata.find("svg")
-groups = svgdata.find_all('g')
+with open(filename, 'r') as file:
+	svg_data = file.read()
+	
+svg = SvgHandle(svg_data)
 
-n_coordinates = 50
-
-fieldnames = ['County', 'x', 'y', 'path']
-with open('county_coordinates.csv', 'w', encoding='utf-8') as csvfile:
-	writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-	writer.writeheader()
-	for group in groups:
-		county = group['id']
-		t = SvgTransform(group['transform'])
-		path_tag = group.find('path')
-		t.add_transform(text=path_tag['transform'])
-		path = SvgPath(pathdata=path_tag['d'], transform=t)
-		coordinates = path.get_coordinates(n_coordinates)
-		path.plot(n_coordinates)
-		for index, (x, y) in enumerate(coordinates):
-			entry = dict()
-			entry['County'] = county
-			entry['x'] = x
-			entry['y'] = y
-			entry['path'] = index+1
-			writer.writerow(entry)
-			
+svg.plot(500)
 plt.show()
-plt.close()
+plt.close('all')
